20b5e1afe2eab897e8f2efd86597007da87c523a
suszak <matys.1996@wp.pl>
2020-06-08 16:51:02 +0200

    Updates

---
+++ precache-manifest.9096cc7e2b7b58336ac823d720007ddb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2568d8e90048fbdeecec5c1b6eac1b9",
+    "url": "/companies-income/index.html"
+  },
+  {
+    "revision": "455de74588a141020f9b",
+    "url": "/companies-income/static/css/main.32900d7b.chunk.css"
+  },
+  {
+    "revision": "12e54174080d626b6cce",
+    "url": "/companies-income/static/js/2.c6db5626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/companies-income/static/js/2.c6db5626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455de74588a141020f9b",
+    "url": "/companies-income/static/js/main.629ac3ec.chunk.js"
+  },
+  {
+    "revision": "4605ed0bed1d71bda7a9",
+    "url": "/companies-income/static/js/runtime-main.c91d11a8.js"
+  },
+  {
+    "revision": "ebd3b92b922ffa030e0bc8c7286d37ab",
+    "url": "/companies-income/static/media/Jost-VariableFont_wght.ebd3b92b.ttf"
+  },
+  {
+    "revision": "0f1470d0204b082d563ee98f9853cb96",
+    "url": "/companies-income/static/media/jost-variablefont_italwght-webfont.0f1470d0.woff"
+  },
+  {
+    "revision": "141de5041dbc2fa3a5e031e752a59136",
+    "url": "/companies-income/static/media/jost-variablefont_italwght-webfont.141de504.woff2"
+  },
+  {
+    "revision": "2fbc93283c86eca73a1624a3c2f3bcdb",
+    "url": "/companies-income/static/media/jost-variablefont_italwght-webfont.2fbc9328.svg"
+  },
+  {
+    "revision": "45410f04722a2ae0ffcdbd3f4f6f4e09",
+    "url": "/companies-income/static/media/jost-variablefont_italwght-webfont.45410f04.eot"
+  },
+  {
+    "revision": "5c3c13c7a2fb4d6051b0c38761d4fc4a",
+    "url": "/companies-income/static/media/open white.5c3c13c7.svg"
+  }
+]);
