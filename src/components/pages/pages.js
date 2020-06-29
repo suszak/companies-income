4d262a9d0fdc989ead90b294bee4a81c64b049b4
suszak <matys.1996@wp.pl>
2020-06-29 11:53:17 +0200

    Pagination change - now showing max 5 pages

--- src/components/pages/pages.js
+++ src/components/pages/pages.js
@@ -4,24 +4,16 @@ import './pages.css'
 const Pages = ({ currentPage, totalPages, paginate }) => {
     const pageNumbers = []
     if(totalPages >= 5) {
-      if(currentPage - 1 > 1 && currentPage + 1 < totalPages) {
-          pageNumbers.push(1)
-          pageNumbers.push('...')
-          for(let i = currentPage - 1; i <= currentPage + 1; i++) {
+      if(currentPage - 2 > 1 && currentPage + 2 < totalPages) {
+          for(let i = currentPage - 2; i <= currentPage + 2; i++) {
               pageNumbers.push(i)
           }
-          pageNumbers.push('...')
-          pageNumbers.push(totalPages)
-      } else if(currentPage - 1 <= 1 && currentPage + 1 < totalPages) {
-          for(let i = 1; i <= currentPage + 1; i++) {
+      } else if(currentPage - 2 <= 1 && currentPage + 1 < totalPages) {
+          for(let i = 1; i <= 5; i++) {
               pageNumbers.push(i)
           }
-          pageNumbers.push('...')
-          pageNumbers.push(totalPages)
-      } else if(currentPage - 1 > 1 && currentPage + 1 >= totalPages) {
-          pageNumbers.push(1)
-          pageNumbers.push('...')
-          for(let i = currentPage - 1; i <= totalPages; i++) {
+      } else if(currentPage - 2 > 1 && currentPage + 2 >= totalPages) {
+          for(let i = totalPages - 4; i <= totalPages; i++) {
               pageNumbers.push(i)
           }
       }
@@ -35,7 +27,6 @@ const Pages = ({ currentPage, totalPages, paginate }) => {
       <nav>
         <ul className='pages'>
           {pageNumbers.map((number, index) => (
-            (number==='...')?(<li key={9999-index} className='pages__number dots'>{number}</li>): 
             <li onClick={() => paginate(number)} key={number} className={(number===currentPage)?'pages__number pages__number--current':'pages__number'}>
               <p className={(number===currentPage)?'pages__item pages__item--current':'pages__item'}>{number}</p>
             </li>
@@ -44,5 +35,5 @@ const Pages = ({ currentPage, totalPages, paginate }) => {
       </nav>
     );
   };
-  
+
   export default Pages;
